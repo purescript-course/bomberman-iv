78ad0af03c114a98ad42a403431a02516ded059c
Emedex007 <ivo.salomon@seznam.cz>
2021-12-08 23:14:04 +0100

    refactor bombs and a few small tweaks

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,7 @@
 module Main
   ( Tile
   , World
+  , bombTick
   , draw
   , evenWallPlacement
   , handleEvent
@@ -10,28 +11,26 @@ module Main
   , main
   , movePlayer
   , reactor
+  , setTile
   , width
   )
   where
 
 
-import Data.Array
-import Debug
-import Prelude
-import Reactor.Graphics.Colors
 
-import Data.Array (modifyAt)
+
+import Prelude (class Eq, Unit, bind, const, discard, mod, negate, not, otherwise, when, ($), (&&), (+), (-), (/), (/=), (<), (<$>), (==), (>=), (||))
+import Reactor.Graphics.Colors (blue400, blue600, gray300, green600, red600, yellow700)
 import Data.Grid (Grid, Coordinates)
 import Data.Grid as Grid
-import Data.Int.Bits (xor)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Reactor (Reactor, executeDefaultBehavior, getW, runReactor, updateW_)
 import Reactor.Events (Event(..))
 import Reactor.Graphics.Drawing (Drawing, drawGrid, fill, tile)
 import Reactor.Reaction (Reaction)
-import Web.HTML.Event.BeforeUnloadEvent.EventTypes (beforeunload)
-import Web.HTML.Event.EventTypes (offline)
+
+
 
 width :: Int
 width = 18
@@ -46,7 +45,7 @@ timer = 3
 main :: Effect Unit
 main = runReactor reactor { title: "Bomberman", width, height }
 
-data Tile = Wall | Box | Bomb{time :: Int}| Empty
+data Tile = Wall | Box | Bomb {time :: Int}| Empty
 
 derive instance tileEq :: Eq Tile
 type World = { player :: Coordinates, board :: Grid Tile }
@@ -56,28 +55,20 @@ isWall { x, y } =
   let 
     isBorder = (x == 0 || x == (width - 1) || y == 0 || y == (height - 1)) -- borders
     
-    isHorizontalWall = 
-      if (width  `mod` 2 /= 0) then 
-        x `mod` 2 == 0 
-      else 
-        evenWallPlacement (x < (width / 2)) x
+    isHorizontalWall = isInnerWall width x
       
-    isVerticalWall = 
-      if (height `mod` 2 /= 0) then 
-        y `mod` 2 == 0 
-      else 
-        evenWallPlacement (y < (height / 2)) y                                                          
+    isVerticalWall = isInnerWall height y                                                       
   in
     isBorder || (isHorizontalWall && isVerticalWall)
+  where
+    isInnerWall dimension point  
+      | dimension `mod` 2 /= 0 = point `mod` 2 == 0 
+      | otherwise = evenWallPlacement (point < (dimension / 2)) point
+
 
 isBox :: Coordinates -> Boolean
-isBox { x, y } = let
-  leftTopCorn = (x < 3 && y < 3)
-  rightTopCorn = (x >= width - 3 && y < 3)
-  leftBottomCorn = (x < 3 && y >= height - 3)
-  rightBottomCorn = (x >= width - 3  && y >= height - 3)
-  in
-    not  (leftTopCorn || rightTopCorn || leftBottomCorn || rightBottomCorn)
+isBox { x, y } = not (x < 4 || x >= width - 4) && (y < 4 || y >= height - 4)
+
 
 evenWallPlacement :: Boolean -> Int -> Boolean
 evenWallPlacement true currentIndex = currentIndex `mod` 2 == 0
@@ -92,10 +83,10 @@ initial = { player: { x:1, y: 1 }, board }
   board = Grid.construct width height setTile
 
 setTile :: { x :: Int, y :: Int} -> Tile
-setTile point = 
-  if isWall point then Wall 
-  else if isBox point then Box 
-  else Empty
+setTile point  
+  | isWall point = Wall 
+  | isBox point = Box 
+  | otherwise = Empty
 
 draw :: World -> Drawing
 draw { player, board } = do
@@ -113,39 +104,33 @@ handleEvent event = do
   case event of
     KeyPress { key: "ArrowLeft" } -> do
       movePlayer {xDiff: -1, yDiff: 0}
-      tickBombs
+      let bumbac = bombTick <$> board
+      updateW_{board: bumbac}
     KeyPress { key: "ArrowRight" } -> do 
       movePlayer {xDiff: 1, yDiff: 0}
-      tickBombs
+      let bumbac = bombTick <$> board
+      updateW_{board: bumbac}
     KeyPress { key: "ArrowDown" } -> do 
       movePlayer {xDiff: 0, yDiff: 1}
-      tickBombs
+      let bumbac = bombTick <$> board
+      updateW_{board: bumbac}
     KeyPress { key: "ArrowUp" } -> do 
       movePlayer {xDiff: 0, yDiff: -1}
-      tickBombs
+      let bumbac = bombTick <$> board
+      updateW_{board: bumbac}
     KeyPress { key: " " } -> do 
-      let bumbac = Grid.updateAt' {x,y} (Bomb{time: timer - 1}) board  --idk proč -1 ale takhle to tikne právě 3x, což je timer, idk...
-      tickBombs
-      updateW_ {board: bumbac}
+      let placeBomb = Grid.updateAt' {x,y} (Bomb{time: timer}) board 
+      let bumbac = bombTick <$> placeBomb
+      updateW_{board: bumbac}
     --Tick{} -> movePlayer Up
     _ -> executeDefaultBehavior
 
+bombTick :: Tile -> Tile
+bombTick (Bomb {time})
+  |time == 0 = Empty
+  |otherwise = Bomb {time: time - 1}
+bombTick t = t
 
-tickBombs = tickBombss 0 0
-tickBombss x y = do
-  --scrollování řádkama
-  let yy = if x == width - 1 then y + 1 else y
-  let xx = if x == width - 1 then 0 else x
-
-  { board } <- getW
-  case Grid.index board {x: xx, y:yy} of
-    Nothing -> executeDefaultBehavior -- vlastně default case v rekurzi, ono to vrátí nothing když jsem out of bounds
-    Just a -> case a of
-      Bomb{time} -> do 
-        let sakumprask = if time == 0 then Grid.updateAt' {x: xx,y: yy} Empty board else Grid.updateAt' {x: xx,y: yy} (Bomb{time: time - 1}) board
-        updateW_ {board: sakumprask}
-        tickBombss (xx + 1) yy
-      _ -> tickBombss (xx + 1) yy -- checkne celý grid
 movePlayer :: {xDiff :: Int, yDiff :: Int} -> Reaction World
 movePlayer {xDiff, yDiff} = do
   {player: { x, y }, board } <- getW
@@ -154,16 +139,3 @@ movePlayer {xDiff, yDiff} = do
     updateW_ { player: newPlayerPosition }
   where
   isEmpty position board = Grid.index board position == Just Empty
-
--- movePlayer direction = do
---   { player: { x, y }, board } <- getW
---   let newPlayerPosition = case direction of
---         Left -> { x: x -1, y: y}
---         Right -> { x: x + 1, y: y}
---         Down -> { x: x, y: y + 1}
---         Up -> { x: x, y: y - 1}
---   when (isEmpty newPlayerPosition board) $
---     updateW_ { player: newPlayerPosition }
---   where
---   isEmpty position board = Grid.index board position == Just Empty
-
